b67cbf9be1454a68e5ea419b7885413df4d36025
mrbesher <mrbesheralkurdi@outlook.com>
2023-01-02 02:24:22 +0300

    Add helpers

---
+++ helpers/facemesh.py
@@ -0,0 +1,126 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_face_mesh = mp.solutions.face_mesh
+
+
+class FaceMesh:
+    # Landmarks
+    LEFT_TOP = 386
+    LEFT_BOTTOM = 374
+    LEFT_INNEl_H = 362
+    LEFT_OUTEl_H = 263
+
+    RIGHT_TOP = 159
+    RIGHT_BOTTOM = 145
+    RIGHT_OUTEl_H = 33
+    RIGHT_INNEl_H = 133
+
+    LEFT_IRIS_C = 473
+    RIGHT_IRIS_C = 468
+
+    MESH_TOP = 10
+    MESH_BOTTOM = 152
+
+    def __init__(self):
+        self.face_mesh = mp_face_mesh.FaceMesh(
+            max_num_faces=1,
+            refine_landmarks=True,
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5)
+        self.image = None
+        self.face_landmarks = None
+
+    def __entel__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.face_mesh.__exit__(type, value, traceback)
+
+    def _landmark_to_array(self, landmark):
+        if type(landmark) is not np.ndarray:
+            return np.array([landmark.x, landmark.y, landmark.z])
+        return landmark
+
+    def calc_euclidean(self, landmark1, landmark2):
+        landmark1 = self._landmark_to_array(landmark1)
+        landmark2 = self._landmark_to_array(landmark2)
+        return np.linalg.norm(landmark1 - landmark2)
+
+    def set_image(self, image):
+        self.image = image
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = self.face_mesh.process(image)
+        if results.multi_face_landmarks:
+            self.face_landmarks = results.multi_face_landmarks[0]
+        else:
+            self.face_landmarks = None
+
+    def get_annotated_image(self):
+        if self.image is None:
+            return None
+        
+        image = self.image.copy()
+        image.flags.writeable = True
+        if self.face_landmarks is None:
+            return image
+
+        mp_drawing.draw_landmarks(
+            image=image,
+            landmark_list=self.face_landmarks,
+            connections=mp_face_mesh.FACEMESH_IRISES,
+            landmark_drawing_spec=None,
+            connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_iris_connections_style())
+
+        return image
+
+
+    def get_distances(self):
+        if self.face_landmarks is None:
+            return None
+
+        landmarks = self.face_landmarks.landmark
+
+        # use two points for scaling from the outer mesh
+        reference_dist = self.calc_euclidean(landmarks[self.MESH_TOP], landmarks[self.MESH_BOTTOM])
+
+        l_eyelid_dist = self.calc_euclidean(
+            landmarks[self.LEFT_TOP], landmarks[self.LEFT_BOTTOM])
+        l_inner_dist = self.calc_euclidean(
+            landmarks[self.LEFT_INNEl_H], landmarks[self.LEFT_IRIS_C])
+        l_outer_dist = self.calc_euclidean(
+            landmarks[self.LEFT_OUTEl_H], landmarks[self.LEFT_IRIS_C])
+        l_top_dist = self.calc_euclidean(
+            landmarks[self.LEFT_TOP], landmarks[self.LEFT_IRIS_C])
+        l_bottom_dist = self.calc_euclidean(
+            landmarks[self.LEFT_BOTTOM], landmarks[self.LEFT_IRIS_C])
+
+        r_eyelid_dist = self.calc_euclidean(
+            landmarks[self.RIGHT_TOP], landmarks[self.RIGHT_BOTTOM])
+        r_inner_dist = self.calc_euclidean(
+            landmarks[self.RIGHT_INNEl_H], landmarks[self.RIGHT_IRIS_C])
+        r_outer_dist = self.calc_euclidean(
+            landmarks[self.RIGHT_OUTEl_H], landmarks[self.RIGHT_IRIS_C])
+        r_top_dist = self.calc_euclidean(
+            landmarks[self.RIGHT_TOP], landmarks[self.RIGHT_IRIS_C])
+        r_bottom_dist = self.calc_euclidean(
+            landmarks[self.RIGHT_BOTTOM], landmarks[self.RIGHT_IRIS_C])
+
+        distances = {
+            'l_eyelid_dist': l_eyelid_dist,
+            'l_inner_dist': l_inner_dist,
+            'l_outer_dist': l_outer_dist,
+            'l_top_dist': l_top_dist,
+            'l_bottom_dist': l_bottom_dist,
+            'r_eyelid_dist': r_eyelid_dist,
+            'r_inner_dist': r_inner_dist,
+            'r_outer_dist': r_outer_dist,
+            'r_top_dist': r_top_dist,
+            'r_bottom_dist': r_bottom_dist
+        }
+
+        # Return the scaled distances
+        return {k: dist / reference_dist for k, dist in distances.items()}
---
+++ helpers/gaze_tracking.py
@@ -0,0 +1,140 @@
+import collections
+import statistics
+
+from enum import Enum
+from math import isclose
+from helpers.facemesh import FaceMesh
+
+def init_distances_dict():
+        return {
+            'r_eyelid_dist': [],
+            'r_inner_dist': [],
+            'r_outer_dist': [],
+            'r_top_dist': [],
+            'r_bottom_dist': [],
+            'l_eyelid_dist': [],
+            'l_inner_dist': [],
+            'l_outer_dist': [],
+            'l_top_dist': [],
+            'l_bottom_dist': []
+        }
+
+def cmp_with_tol(a, b, tol=6e-2):
+    if isclose(a, b, rel_tol=tol):
+        return 0
+
+    return a - b
+
+class GazeDirection(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
+    CENTER = 5
+    UNKNOWN = 6
+    NO_FACE = 7
+
+class GazeTracker:
+    def __init__(self, num_calibration_frames: int=20, buffer_size: int=20):
+        self.annotated_frame = None
+        self.num_calibration_frames = num_calibration_frames
+        self.buffer_size = buffer_size
+        self.calibration_frame_count = 0
+        self.is_calibrating = True
+        self.fm = FaceMesh()
+        self.distances = init_distances_dict()
+        self.median_distances = {}
+        self.gaze_buffer = collections.deque(maxlen=buffer_size)
+
+    def _update_calibration(self, frame_distances):
+        for k, dist in frame_distances.items():
+            self.distances[k].append(dist)
+            self.calibration_frame_count += 1
+
+            if self.calibration_frame_count >= self.num_calibration_frames:
+                self.median_distances = {distance_name: statistics.median(distance_values)
+                                        for distance_name, distance_values in self.distances.items()}
+                self.is_calibrating = False
+                self.calibration_frame_count = 0
+                self.distances = init_distances_dict()
+
+
+    def update(self, frame):
+        """Updates the gaze tracker with a new frame.
+
+        Args:
+            frame: a frame from a video stream
+        """
+        # update the gaze tracker and annotate the frame
+        self.fm.set_image(frame)
+        self.annotated_frame = self.fm.get_annotated_image()
+        frame_distances = self.fm.get_distances()
+
+        if frame_distances is None:
+            self.gaze_buffer.append(GazeDirection.NO_FACE)
+            return
+
+        if self.is_calibrating:
+            self._update_calibration(frame_distances)
+            return
+        
+        # right eye
+        if cmp_with_tol(frame_distances['r_inner_dist'], self.median_distances['r_inner_dist'], tol=1e-1) < 0:
+            self.gaze_buffer.append(GazeDirection.LEFT)
+        elif cmp_with_tol(frame_distances['r_outer_dist'], self.median_distances['r_outer_dist'], tol=1e-1) < 0:
+            self.gaze_buffer.append(GazeDirection.RIGHT)
+        elif cmp_with_tol(frame_distances['r_eyelid_dist'], self.median_distances['r_eyelid_dist']) < 0 \
+            and cmp_with_tol(frame_distances['r_top_dist'], self.median_distances['r_top_dist']) < 0:
+            self.gaze_buffer.append(GazeDirection.DOWN)
+        elif cmp_with_tol(frame_distances['r_eyelid_dist'], self.median_distances['r_eyelid_dist']) > 0 \
+            and cmp_with_tol(frame_distances['r_bottom_dist'], self.median_distances['r_bottom_dist']) > 0:
+            self.gaze_buffer.append(GazeDirection.UP)
+        else:
+            self.gaze_buffer.append(GazeDirection.CENTER)
+
+        # left eye
+        if cmp_with_tol(frame_distances['l_outer_dist'], self.median_distances['l_outer_dist'], tol=1e-1) < 0:
+            self.gaze_buffer.append(GazeDirection.LEFT)
+        elif cmp_with_tol(frame_distances['l_inner_dist'], self.median_distances['l_inner_dist'], tol=1e-1) < 0:
+            self.gaze_buffer.append(GazeDirection.RIGHT)
+        elif cmp_with_tol(frame_distances['l_eyelid_dist'], self.median_distances['l_eyelid_dist']) < 0 \
+            and cmp_with_tol(frame_distances['l_top_dist'], self.median_distances['l_top_dist']) < 0:
+            self.gaze_buffer.append(GazeDirection.DOWN)
+        elif cmp_with_tol(frame_distances['l_eyelid_dist'], self.median_distances['l_eyelid_dist']) > 0 \
+            and cmp_with_tol(frame_distances['l_bottom_dist'], self.median_distances['l_bottom_dist']) > 0:
+            self.gaze_buffer.append(GazeDirection.UP)
+        else:
+            self.gaze_buffer.append(GazeDirection.CENTER)
+
+
+    def calibrate(self):
+        self.is_calibrating = True
+
+    def get_gaze_direction(self):
+        """Returns the current gaze direction.
+
+        Returns:
+            an instance of GazeDirection
+        """
+        if self.is_calibrating:
+            return GazeDirection.UNKNOWN
+        
+        if len(self.gaze_buffer) < self.buffer_size:
+            return GazeDirection.UNKNOWN
+
+        direction_counts = collections.Counter(self.gaze_buffer)
+
+        mode, occurrence = direction_counts.most_common(1)[0]
+
+        if occurrence < self.buffer_size // 2:
+            return GazeDirection.UNKNOWN
+
+        return mode
+
+    def get_last_annotated_frame(self):
+        """Returns the last annotated frame.
+
+        Returns:
+            an annotated frame (ndarray)
+        """
+        return self.annotated_frame
