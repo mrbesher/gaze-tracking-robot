4c92fc83b5a82b32d8dae1a982cd91a180982f4b
mrbesher <mrbesheralkurdi@outlook.com>
2023-01-02 17:47:22 +0300

    Remove threading, refine the code

--- helpers/robot_control.py
+++ helpers/robot_control.py
@@ -18,13 +18,13 @@ class RobotController:
         """
         self.ip_address = ip_address
 
-    def send_command(self, command: Union[str, Command], duration :int=1000, velocity: int=150):
+    def send_command(self, command: Union[str, Command], duration :int=100, velocity: int=150):
         """Sends a command to the robot to execute.
 
         Args:
             command (Union[str, Command]): The command to send to the robot. Must be a string or an instance of the
                 Command enum.
-            duration (int, optional): The duration in milliseconds to execute the command. Defaults to 1000.
+            duration (int, optional): The duration in milliseconds to execute the command. Defaults to 100.
             velocity (int, optional): The velocity at which to execute the command. Defaults to 150.
         """
         if isinstance(command, Command):
@@ -38,17 +38,17 @@ class RobotController:
         }
         requests.get(url, params=params)
 
-    def move_forward(self, duration: int=1000, velocity: int=150):
+    def move_forward(self, duration: int=100, velocity: int=150):
         self.send_command(Command.MF, duration=duration, velocity=velocity)
 
-    def move_backward(self, duration: int=1000, velocity: int=150):
+    def move_backward(self, duration: int=100, velocity: int=150):
         self.send_command(Command.MB, duration=duration, velocity=velocity)
 
-    def turn_right(self, duration: int=1000, velocity: int=150):
+    def turn_right(self, duration: int=100, velocity: int=150):
         self.send_command(Command.TR, duration=duration, velocity=velocity)
 
-    def turn_left(self, duration: int=1000, velocity: int=150):
+    def turn_left(self, duration: int=100, velocity: int=150):
         self.send_command(Command.TL, duration=duration, velocity=velocity)
 
-    def park_robot(self, duration: int=1000, velocity: int=150):
+    def park_robot(self, duration: int=100, velocity: int=150):
         self.send_command(Command.P, duration=duration, velocity=velocity)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import threading
 from time import sleep
@@ -6,8 +7,12 @@ import customtkinter
 
 from PIL import Image
 from helpers.gaze_tracking import GazeTracker, GazeDirection
+from helpers.robot_control import RobotController
 
-TICK_INTERVAL = 1 / 30 # FPS
+TICK_INTERVAL = int(1 / 30 * 1000) # ms
+
+BLUE_COLOR = '#003366'
+DARKBLUE_COLOR = '#032140'
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -15,12 +20,12 @@ class App(customtkinter.CTk):
 
         self.title('Gaze Robot')
 
-        # set grid layout 1x2
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
 
+        self.camera_control = True
         # Init GazeTracker object and open video capture
-        self.gt = GazeTracker()
+        self.gt = GazeTracker(num_calibration_frames=200)
         self.cap = cv2.VideoCapture(0)
 
         while self.cap.isOpened():
@@ -37,46 +42,46 @@ class App(customtkinter.CTk):
         img = Image.fromarray(frame)
         self.image = customtkinter.CTkImage(img, size=(width, height))
 
-        # create home frame
         self.frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color='transparent')
         self.frame.grid_columnconfigure(0, weight=1)
 
-        self.message_label = customtkinter.CTkLabel(self.frame, text='Initializing', bg_color='#003366')
+        self.message_label = customtkinter.CTkLabel(self.frame, text='Initializing', bg_color=BLUE_COLOR)
         self.message_label.grid(row=0, columnspan=3, sticky='ew')
 
         self.image_label = customtkinter.CTkLabel(self.frame, text='', image=self.image)
         self.image_label.grid(row=1, column=0, columnspan=3)
 
-        self.forward_button = customtkinter.CTkButton(self.frame, text='Forward', fg_color='green', hover_color='darkgreen')
+        self.forward_button = customtkinter.CTkButton(self.frame, text='Forward', fg_color=BLUE_COLOR, hover_color=DARKBLUE_COLOR, command=rc.move_forward)
         self.forward_button.grid(row=2, column=1, padx=20, pady=10)
 
-        self.left_button = customtkinter.CTkButton(self.frame, text='Left', fg_color='green', hover_color='darkgreen')
+        self.left_button = customtkinter.CTkButton(self.frame, text='Left', fg_color=BLUE_COLOR, hover_color=DARKBLUE_COLOR, command=rc.turn_left)
         self.left_button.grid(row=3, column=0, padx=20, pady=10)
 
         self.park_button = customtkinter.CTkButton(self.frame, text='Park', fg_color='darkred', hover_color='#670000')
         self.park_button.grid(row=3, column=1, padx=20, pady=10)
 
-        self.right_button = customtkinter.CTkButton(self.frame, text='Right', fg_color='green', hover_color='darkgreen')
+        self.right_button = customtkinter.CTkButton(self.frame, text='Right', fg_color=BLUE_COLOR, hover_color=DARKBLUE_COLOR, command=rc.turn_right)
         self.right_button.grid(row=3, column=2, padx=20, pady=10)
 
-        self.calibrate_button = customtkinter.CTkButton(self.frame, text='Calibrate', fg_color='#d8951a', hover_color='#c07d01')
+        self.calibrate_button = customtkinter.CTkButton(self.frame, text='Calibrate', fg_color='#d8951a', hover_color='#c07d01', command=self.gt.calibrate)
         self.calibrate_button.grid(row=4, column=0, padx=20, pady=10)
 
-        self.backward_button = customtkinter.CTkButton(self.frame, text='Backward', fg_color='green', hover_color='darkgreen')
+        self.backward_button = customtkinter.CTkButton(self.frame, text='Backward', fg_color=BLUE_COLOR, hover_color=DARKBLUE_COLOR, command=rc.move_backward)
         self.backward_button.grid(row=4, column=1, padx=20, pady=10)
 
-        self.backward_button = customtkinter.CTkButton(self.frame, text='Toggle Control', fg_color='#d8951a', hover_color='#c07d01')
+        self.backward_button = customtkinter.CTkButton(self.frame, text='Toggle Control', fg_color='#d8951a', hover_color='#c07d01', command=self.toggle_control)
         self.backward_button.grid(row=4, column=2, padx=20, pady=10)
 
         # select default frame
         self.frame.grid(row=0, column=1, sticky='nsew')
         
         self.stop_event = threading.Event()
-        self.thread = threading.Thread(target=self.video_loop)
-        self.thread.start()
+        self.frame.after(TICK_INTERVAL, self.update_image)
+
 
     def video_loop(self):
         while not self.stop_event.is_set():
+            sleep(TICK_INTERVAL / 1000)
             self.update_image()
 
     def update_image(self):
@@ -108,20 +113,58 @@ class App(customtkinter.CTk):
         self.image_label.image = image
 
         # Update direction label text and color
-        if direction == GazeDirection.NO_FACE:
-            self.message_label.configure(text='Direction: No Face', bg_color=('red', 'red'))
+        status_text = '[Enabled]' if self.camera_control else '[Disabled]'
+
+        if self.gt.is_calibrating:
+            self.message_label.configure(text=f'Calibrating\n{status_text}', bg_color=BLUE_COLOR)
+        elif direction == GazeDirection.NO_FACE:
+            self.message_label.configure(text=f'No Face\n{status_text}', bg_color='darkred')
         elif direction == GazeDirection.UNKNOWN:
-            self.message_label.configure(text='Direction: Unknown', bg_color=('#d8951a', '#d8951a'))
+            self.message_label.configure(text=f'Direction: Unknown\n{status_text}', bg_color='#d8951a')
         else:
-            self.message_label.configure(text=f'Direction: {direction.name}', bg_color=('green', 'green'))
+            self.message_label.configure(text=f'Direction: {direction.name}\n{status_text}', bg_color='green')
+            if self.camera_control:
+                gaze_control_mapping[direction](duration=TICK_INTERVAL)
+
+        self.frame.after(TICK_INTERVAL, self.update_image)
+
 
     def on_closing(self, *kwargs):
-        self.stop_event.set()
         self.cap.release()
-        self.thread.join()
         self.destroy()
 
+    def calibrate_button_event(self):
+        self.gt.calibrate()
+
+    def toggle_control(self):
+        self.camera_control = not self.camera_control
+
+
+class DummyRobotController(RobotController):
+    def send_command(self, *args, **kwargs):
+        pass
+    
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('ip', type=str, help='The IP address of the robot')
+    parser.add_argument('--dry-run', action='store_true', help='Do not send commands to the robot')
+    args = parser.parse_args()
+
+    if args.dry_run:
+        rc = DummyRobotController(args.ip)
+        print('Dry run: commands will not be sent to the robot')
+    else:
+        rc = RobotController(args.ip)
+
+    gaze_control_mapping = {
+        GazeDirection.UP: rc.move_forward,
+        GazeDirection.DOWN: rc.move_backward,
+        GazeDirection.RIGHT: rc.turn_right,
+        GazeDirection.LEFT: rc.turn_left,
+        GazeDirection.CENTER: rc.park_robot,
+    }
+
     app = App()
     app.protocol('WM_DELETE_WINDOW', app.on_closing)
     app.bind('<Escape>', app.on_closing)
